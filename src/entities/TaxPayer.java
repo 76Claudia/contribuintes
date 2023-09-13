bc094c64d993d98249920e16fd2f6f226e4534f5
76Claudia <claudinha.gcruz@gmail.com>
2023-09-12 22:29:15 -0300

    Projeto criado

---
+++ src/entities/TaxPayer.java
@@ -0,0 +1,69 @@
+package entities;
+
+public class TaxPayer {
+	
+	private double salaryIncome;
+	private double serviceIncome;
+	private double capitalIncome;
+	private double healthSpending;
+	private double educationSpending;
+	
+	public TaxPayer(double salaryIncome, double serviceIncome, double capitalIncome, double healthSpending,
+			double educationSpending) {
+		
+		this.salaryIncome = salaryIncome;
+		this.serviceIncome = serviceIncome;
+		this.capitalIncome = capitalIncome;
+		this.healthSpending = healthSpending;
+		this.educationSpending = educationSpending;
+	}
+
+	public double getSalaryIncome() {
+		return salaryIncome;
+	}
+
+	public void setSalaryIncome(double salaryIncome) {
+		this.salaryIncome = salaryIncome;
+	}
+
+	public double getServiceIncome() {
+		return serviceIncome;
+	}
+
+	public void setServiceIncome(double serviceIncome) {
+		this.serviceIncome = serviceIncome;
+	}
+
+	public double getCapitalIncome() {
+		return capitalIncome;
+	}
+
+	public void setCapitalIncome(double capitalIncome) {
+		this.capitalIncome = capitalIncome;
+	}
+
+	public double getHealthSpending() {
+		return healthSpending;
+	}
+
+	public void setHealthSpending(double healthSpending) {
+		this.healthSpending = healthSpending;
+	}
+
+	public double getEducationSpending() {
+		return educationSpending;
+	}
+
+	public void setEducationSpending(double educationSpending) {
+		this.educationSpending = educationSpending;
+	}
+	
+	public double salaryTax() {
+		if(salaryIncome > 3000) {
+			return  
+		}
+	}
+	
+	
+
+}
