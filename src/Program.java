647040c27aaf2d0dc0a5b89ae4ffd5942d55f73d
76Claudia <claudinha.gcruz@gmail.com>
2023-09-14 22:58:37 -0300

    Criados os métodos e lista de contribuintes

--- src/Program.java
+++ src/Program.java
@@ -16,19 +16,24 @@ public class Program {
 		
 		System.out.print("Quantos contribuintes voce vai digitar? ");
 		int N = sc.nextInt();
+		System.out.println();
 		
 		for(int i=0; i<N; i++) {
 			System.out.println("Digite os dados do " + (i+1) + "o" +" contribuinte: ");
 			System.out.print("Renda anual com salario: ");
 			double salaryIncome = sc.nextDouble();
 			System.out.print("Renda anual com prestacao de servico: ");
-			double serviceIncome = sc.nextDouble();
+			double servicesIncome = sc.nextDouble();
 			System.out.print("Renda anual com ganho de capital: ");
 			double capitalIncome = sc.nextDouble();
 			System.out.print("Gastos medicos: ");
 			double healthSpending = sc.nextDouble();
 			System.out.print("Gastos educacionais: ");
 			double educationSpending = sc.nextDouble();
+			
+			TaxPayer tax =  new TaxPayer(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationSpending);
+			
+			list.add(tax);
 			
 			
 		}
--- src/entities/TaxPayer.java
+++ src/entities/TaxPayer.java
@@ -3,16 +3,19 @@ package entities;
 public class TaxPayer {
 	
 	private double salaryIncome;
-	private double serviceIncome;
+	private double servicesIncome;
 	private double capitalIncome;
 	private double healthSpending;
 	private double educationSpending;
 	
-	public TaxPayer(double salaryIncome, double serviceIncome, double capitalIncome, double healthSpending,
+	public TaxPayer() {
+		
+	}
+	public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending,
 			double educationSpending) {
 		
 		this.salaryIncome = salaryIncome;
-		this.serviceIncome = serviceIncome;
+		this.servicesIncome = servicesIncome;
 		this.capitalIncome = capitalIncome;
 		this.healthSpending = healthSpending;
 		this.educationSpending = educationSpending;
@@ -27,11 +30,11 @@ public class TaxPayer {
 	}
 
 	public double getServiceIncome() {
-		return serviceIncome;
+		return servicesIncome;
 	}
 
 	public void setServiceIncome(double serviceIncome) {
-		this.serviceIncome = serviceIncome;
+		this.servicesIncome = serviceIncome;
 	}
 
 	public double getCapitalIncome() {
@@ -57,5 +60,54 @@ public class TaxPayer {
 	public void setEducationSpending(double educationSpending) {
 		this.educationSpending = educationSpending;
 	}
+	
+	public double salaryTax() {
+		double monthtlyIncome = salaryIncome / 12.0;
+		if(salaryIncome < 3000.0) {
+			double salaryTax = 0.0;
+		}
+		else if(salaryIncome < 5000.0) {
+			double salaryTax = salaryIncome * 0.1;
+			
+			}
+		
+		else {
+			 double salaryTax = salaryIncome * 0.2;
+		}
+		     return salaryTax();
+		
+		
+	}	
+	public double servicesTax() {
+		return  servicesIncome * 0.15;
+	}
+	
+	public double capitalTax() {
+		return capitalIncome * 0.2;
+	}
 
+	public double grossTax() {
+		return salaryTax() + servicesTax() + capitalTax();
+	}
+	public double taxRebate() {
+	 double maximumDeductible = grossTax() * 0.3;
+
+	double deductibleExpenses = 0;
+	if(deductibleExpenses > maximumDeductible) {
+		return maximumDeductible;
+	}
+	else 
+		return deductibleExpenses;
+	}
+	public double netTax() {
+		return grossTax() - taxRebate();
+			
+	}
+	
+	
+	
 }
+
+
+	
+
